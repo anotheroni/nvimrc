89633a7adc42a17a0f5f86e851c71da6d49da67a
Oskar Nilsson <oskar@rums.se>
2025-02-24 14:04:01 +0100

    Add basedpyright for Python development

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -154,7 +154,8 @@ return {
 		-- for you, so that they are available from within Neovim.
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
-			'clangd',
+			'basedpyright', -- python
+			'clangd', -- C lsp
 			'clang-format',
 			'codelldb', -- C DAP
 			'shellcheck', -- bash linter
