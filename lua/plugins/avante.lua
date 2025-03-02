13145dd33c76012d01b8c8357859c32ab933fce2
Oskar Nilsson <oskar@rums.se>
2025-03-02 20:15:15 +0100

    Avante config fixes

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -6,12 +6,12 @@ return {
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
 		--- The below dependencies are optional,
-		--"echasnovski/mini.pick", -- for file_selector provider mini.pick
+		"echasnovski/mini.pick", -- for file_selector provider mini.pick
 		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
 		"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
 		"ibhagwan/fzf-lua", -- for file_selector provider fzf
 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-		"zbirenbaum/copilot.lua", -- for providers='copilot'
+		--"zbirenbaum/copilot.lua", -- for providers='copilot'
 		{
 			-- support for image pasting
 			"HakonHarnes/img-clip.nvim",
@@ -43,22 +43,45 @@ return {
 			event = "VeryLazy",
 			lazy = false,
 			version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-			opts = {
-				-- add any opts here
-				-- for example
-				provider = "openai",
-				openai = {
-					endpoint = "https://api.openai.com/v1",
-					model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-					timeout = 30000, -- timeout in milliseconds
-					temperature = 0, -- adjust if needed
+			build = "make",
+			-- add any opts here
+			provider = "groq_qwen",
+			vendors = {
+				openrouter = {
+					__inherited_from = "openai",
+					api_key_name = "OPENROUTER_API_KEY",
+					endpoint = "https://openrouter.ai/api/v1",
+					model = "deepseek/deepseek-r1",
+				},
+				groq_llama = {
+					__inherited_from = "openai",
+					api_key_name = "GROQ_API_KEY",
+					endpoint = "https://api.groq.com/openai/v1",
+					model = "llama-3.3-70b-versatile",
+				},
+				groq_qwen = {
+					__inherited_from = "openai",
+					api_key_name = "GROQ_API_KEY",
+					endpoint = "https://api.groq.com/openai/v1",
+					model = "qwen-2.5-coder-32b",
 					max_tokens = 4096,
-					-- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+				},
+				ollama = {
+					__inherited_from = "openai",
+					api_key_name = "",
+					endpoint = "https://127.0.01:11434/v1",
+					model = "llama-3.3-70b-versatile",
 				},
 			},
-			-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-			build = "make",
-			-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+--			groq_llama = {
+--				timeout = 30000, -- timeout in milliseconds
+--				temperature = 0, -- adjust if needed
+--				max_tokens = 1024,
+--			},
+--			groq_qwen = {
+--				timeout = 30000, -- timeout in milliseconds
+--				temperature = 0, -- adjust if needed
+--			},
 		})
 	end
 }
